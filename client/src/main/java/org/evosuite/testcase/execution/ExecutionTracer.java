82d0b422ac6e7db992384a5f573cfa7dfdc2d082
Giovanni Denaro <denaro@disco.unimib.it>
2026-07-07 17:30:26 +0200

    minor change

--- client/src/main/java/org/evosuite/testcase/execution/ExecutionTracer.java
+++ client/src/main/java/org/evosuite/testcase/execution/ExecutionTracer.java
@@ -20,7 +20,6 @@
 package org.evosuite.testcase.execution;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
